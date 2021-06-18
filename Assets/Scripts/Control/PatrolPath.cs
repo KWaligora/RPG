1265e5062fa883a70449cada05c93538aff17930
Kamil Waligóra <k.waligora37@gmail.com>
2021-06-18 10:49:14 +0200

    Add guarding behaviour

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
