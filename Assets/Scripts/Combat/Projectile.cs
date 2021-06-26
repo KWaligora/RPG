db79fe1b968265d5f30a198157294ff7b94bdf6c
Kamil Waligóra <k.waligora37@gmail.com>
2021-06-26 19:43:38 +0200

    Add bow with arrow

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,33 @@
+using RPG.Core;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        Health target = null;
+        [SerializeField] float speed = 2f;
+
+        GameObject player;
+
+        private void Update() 
+        {
+            if(target == null) return;
+
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);    
+        }
+
+        public void SetTarget(Health target)
+        {
+            this.target = target;
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+            if(targetCapsule == null) return target.transform.position;
+            return target.transform.position + Vector3.up * targetCapsule.height / 1.5f;
+        }
+    }
+}
