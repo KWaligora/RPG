28b339bd37c591ce22c4601044840749418e8ffa
Kamil Waligóra <k.waligora37@gmail.com>
2021-06-30 11:45:38 +0200

    Add debug health display

--- Assets/Scripts/Resources/HealthDisplay.cs
+++ Assets/Scripts/Resources/HealthDisplay.cs
@@ -15,7 +15,7 @@ namespace RPG.Resources
 
         private void Update() 
         {
-                GetComponent<Text>().text = health.GetPercentage() + "%";
+                GetComponent<Text>().text = string.Format("{0:0}%", health.GetPercentage());
         }
     }
 
