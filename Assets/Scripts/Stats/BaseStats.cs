6a57a30464d2f47d9a6dea01c078883158d4f461
Kamil Waligóra <k.waligora37@gmail.com>
2021-07-25 15:06:04 +0200

    Add levelUp

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -8,9 +8,35 @@ namespace RPG.Stats
         [SerializeField] CharacterClass characterClass;  
         [SerializeField] Progression progression = null;
 
+        private void Update() 
+        {
+            if(gameObject.tag == "Player")
+            {
+                print(GetLevel());
+            }    
+        }
+
         public int GetStat(Stat stat)
         {
             return progression.GetStat(stat, characterClass, startingLevel);
+        }
+
+        public int GetLevel()
+        {
+            Experience experience = GetComponent<Experience>();
+            if(experience == null) return startingLevel;
+
+            int currentXP = GetComponent<Experience>().GetPoints();
+            int penultimateLevel = progression.GetLevels(Stat.ExpierenceToLevelUp, characterClass); //penultimate - przedostatni
+            for(int level = 1; level < penultimateLevel; level++)
+            {
+                int XPToLevelUp = progression.GetStat(Stat.ExpierenceToLevelUp, characterClass, level);
+                if(XPToLevelUp > currentXP)
+                {
+                    return level;
+                }
+            }
+            return penultimateLevel;
         }
     }
 }
